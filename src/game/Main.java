dd071fc3de9f604925447023039d29ec4ababbbc
Hatesh Saini <Hatesh@Nascenta..com>
2018-11-27 15:52:51 +0000

    more initialising

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main_menu.fxml"));
+        primaryStage.setTitle("Hatesh's Dice Game");
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
 
