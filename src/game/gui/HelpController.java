d4645563544d96643f9ff35590959413ca910d07
Hatesh Saini <hateshsaini1998@gmail.com>
2018-12-08 23:36:07 +0000

    Update HelpController.java

--- src/game/gui/HelpController.java
+++ src/game/gui/HelpController.java
@@ -19,11 +19,11 @@ public class HelpController {
         String s = "Welcome to the Dice Game!";
         s = append(s, "In this game we have two players, each can set their own name.");
         s = append(s, "Next a maximum score needs to be decided so a winner can be found (30 is the default).");
-        s = append(s, "After the game has been setup we can begin to play!");
+        s = append(s, "After the game has been set up we can begin to play!");
         s = append(s, "The game is simple, and goes as follows:");
         s = append(s, "* Each Player takes it in turns to roll the dice");
         s = append(s, " -> 3 Dice are rolled every time");
-        s = append(s, " -> If none of the dice share a value, you get onl 1 point");
+        s = append(s, " -> If none of the dice share a value, you get only 1 point");
         s = append(s, " -> If you roll a pair, you get the value of that pair (E.g. A pair of 4's scores 8 points)");
         s = append(s, " -> If you roll 3 of a kind, you score 18 points, regardless of the value!");
         s = append(s, "* The first player to surpass the maximum wins the game!");
